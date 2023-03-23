bb211393215cd7b774873956147ee716c39ee59f
Chaudhary-shruti <93135302+Chaudhary-shruti@users.noreply.github.com>
2023-03-23 09:41:13 -0700

    Add files via upload

---
+++ hand gestures/main.js
@@ -0,0 +1,29 @@
+prediction_1=""
+prediction_2=""
+data_uri=""
+function preload(){
+
+}
+function setUp(){
+
+}
+function draw(){
+
+}
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+})
+camera = document.getElementById("camera")
+Webcam.attach('#camera')
+
+function take_snapshot() {
+    Webcam.snap(
+        function (data_uri) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">'
+        }
+
+    )
+}
